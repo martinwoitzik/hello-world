dee43eaf1dbf4b6fbfc6e864737b8dde41ee498f
Martin Woitzik <martin.woitzik@tresmo.de>
2013-07-09 15:58:03 +0200

    Add saucelab configuration to test.js Add mocha.opts to increase timeout and change output type

--- test/test.js
+++ test/test.js
@@ -3,48 +3,64 @@
  *  WD Test configuration
  */
 
+var assert = require('chai').assert;
 
-var webdriver = require('wd')
-    , assert = require('assert');
+describe('WD', function(){
 
-var browser = webdriver.remote(
-    "ondemand.saucelabs.com"
-    , 80
-    , "martinwoitzik"
-    , "9eb269c6-a2d2-49b5-af1d-f1eb405f1f46"
-);
+    var webdriver = require('wd')
+        , assert = require('assert');
 
-browser.on('status', function(info){
-    console.log('\x1b[36m%s\x1b[0m', info);
-});
+    var browser = webdriver.remote(
+        "ondemand.saucelabs.com"
+        , 80
+        , "martinwoitzik"
+        , "9eb269c6-a2d2-49b5-af1d-f1eb405f1f46"
+    );
 
-browser.on('command', function(meth, path){
-    console.log(' > \x1b[33m%s\x1b[0m: %s', meth, path);
-});
+    browser.on('status', function(info){
+        console.log('\x1b[36m%s\x1b[0m', info);
+    });
 
-var desired = {
-    browserName: 'iphone'
-    , version: '5.0'
-    , platform: 'Mac 10.6'
-    , tags: ["examples"]
-    , name: "This is an example test"
-}
+    browser.on('command', function(meth, path){
+        console.log(' > \x1b[33m%s\x1b[0m: %s', meth, path);
+    });
 
-browser.init(desired, function() {
-    browser.get("http://hello-world-app.herokuapp.com/", function() {
-        browser.title(function(err, title) {
-            assert.ok(~title.indexOf('Meine kleine Welt App'), 'Wrong title!');
+    var desired = {
+        browserName: 'chrome'
+//        , version: '5.0'
+//        , platform: 'Mac 10.6'
+        , tags: ["examples"]
+        , name: "This is an example test"
+    }
 
-            /*
-            browser.elementById('submit', function(err, el) {
-                browser.clickElement(el, function() {
-                    browser.eval("window.location.href", function(err, href) {
-                        assert.ok(~href.indexOf('login'), 'Wrong URL!');
-                        browser.quit()
-                    })
+
+    it('should not login with wrong credentials', function(callback) {
+
+        browser.init(desired, function() {
+            browser.get("http://hello-world-app.herokuapp.com/", function() {
+                browser.title(function(err, title) {
+                    assert.ok(~title.indexOf('Meine kleine Welt App'), 'Wrong title!');
+
+                    /*
+                     browser.elementById('submit', function(err, el) {
+                     browser.clickElement(el, function() {
+                     browser.eval("window.location.href", function(err, href) {
+                     assert.ok(~href.indexOf('login'), 'Wrong URL!');
+                     browser.quit()
+                     })
+                     })
+                     })
+                     */
+
+                    browser.quit();
+
+                    callback();
                 })
             })
-            */
         })
-    })
-})
+
+    });
+
+
+});
+
