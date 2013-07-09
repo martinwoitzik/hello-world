f878387c4cebdb8e8a8c760ea97474f6e0e6b8d5
Martin Woitzik <martin.woitzik@tresmo.de>
2013-07-09 12:12:45 +0200

    Add news subpage

--- routes/news.js
+++ routes/news.js
@@ -3,15 +3,20 @@
  * GET news page.
  */
 
+var entries = [
+    {date:"12.01.2013", content:"sldkfjsldkf jwelkrj wlejr"},
+    {date:"24.03.2013", content:"Halkjwerljwlekrj wlekjr"}
+];
+
 exports.index = function(req, res){
-  res.render('news', {
-      entries:[
-          {date:"12.01.2013", content:"sldkfjsldkf jwelkrj wlejr"},
-          {date:"24.03.2013", content:"Halkjwerljwlekrj wlekjr"}
-      ]
+  res.render('news/index', {
+      entries:entries
   });
 };
 
 exports.show = function(req, res) {
-  res.render('news/show')
+  res.render('news/show', {
+      title:entries[ req.params.id ].date,
+      content:entries[ req.params.id ].content
+  });
 }
