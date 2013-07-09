f84f9caaba1476c2f6e4b704887cf98063646088
Martin Woitzik <martin.woitzik@tresmo.de>
2013-07-09 15:05:36 +0200

    Changed Login.js to support mocha testing (add boilerplate code) Add amdefine to package.json to support mocha and requirejs

--- test/login.js
+++ test/login.js
@@ -1,19 +1,19 @@
 var assert = require('chai').assert;
 
 var Login = require("../public/js/Login");
-var login = new Login();
-
-describe('Array', function(){
-    describe('#indexOf()', function(){
-        it('should return -1 when the value is not present', function(){
-            assert.equal(-1, [1,2,3].indexOf(5));
-            assert.equal(-1, [1,2,3].indexOf(0));
-        })
-    })
-});
 
 describe('Login', function(){
+
     it('should return false when username/password are incorrect', function() {
-        assert.isTrue( login('wrong-username', 'wrong-password') );
-    })
+        assert.isFalse(
+            Login.validate('wrong-username', 'wrong-password')
+        );
+    });
+
+    it('should return true when username/password are correct', function() {
+        assert.isTrue(
+            Login.validate('martin', 'test')
+        );
+    });
+
 });
